9b5b767ac4e2ae550078f5e46431f0968da08916
Kamil Błoński <kblonski02@gmail.com>
2024-11-29 03:54:58 +0100

    Budowanie nazw selektorów ze znakiem &

--- vue-project/src/components/sass/index.vue
+++ vue-project/src/components/sass/index.vue
@@ -6,7 +6,8 @@
     <!-- <l5_zagniezdzanie></l5_zagniezdzanie> -->
     <!-- <l6_domieszki-zparametrami></l6_domieszki-zparametrami> -->
     <!-- <l7_funkcje></l7_funkcje> -->
-    <l8_zagniezdzanie_pseudoklas></l8_zagniezdzanie_pseudoklas>
+    <!-- <l8_zagniezdzanie_pseudoklas></l8_zagniezdzanie_pseudoklas> -->
+    <l9_tworzenie_nazw_za_pomoca_ampersand></l9_tworzenie_nazw_za_pomoca_ampersand>
 </template>
 
 <script>
@@ -18,6 +19,7 @@ import l5_zagniezdzanie from './l5_zagniezdzanie.vue';
 import L6_domieszkiZparametrami from './l6_domieszkiZparametrami.vue';
 import l7_funkcje from './l7_funkcje.vue';
 import l8_zagniezdzanie_pseudoklas from './l8_zagniezdzanie_pseudoklas.vue';
+import l9_tworzenie_nazw_za_pomoca_ampersand from './l9_tworzenie_nazw_za_pomoca_ampersand.vue';
 
 export default {
     components: {
@@ -28,7 +30,8 @@ export default {
         l5_zagniezdzanie, 
         L6_domieszkiZparametrami,
         l7_funkcje,
-        l8_zagniezdzanie_pseudoklas
+        l8_zagniezdzanie_pseudoklas,
+        l9_tworzenie_nazw_za_pomoca_ampersand
     }
 }
 </script>
---
+++ vue-project/src/components/sass/l9_tworzenie_nazw_za_pomoca_ampersand.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>Tworzenie nazw selektórów za pomocą znaku @ jak w less</div>
+    <div class="table">
+        <div class="row">
+            <div class="cell cell-1-1"></div>
+            <div class="cell cell-2-1"></div>
+            <div class="cell cell-3-1"></div>
+        </div>
+        <div class="row">
+            <div class="cell cell-1-2"></div>
+            <div class="cell cell-2-2"></div>
+            <div class="cell cell-3-2"></div>
+        </div>
+        <div class="row">
+            <div class="cell cell-1-3"></div>
+            <div class="cell cell-2-3"></div>
+            <div class="cell cell-3-3"></div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+$size: 100px;
+$spacing: 2px;
+
+$first-color: red;
+$second-color: green;
+$third-color: blue;
+
+$n: 0.9;
+
+.row {
+    display: block;
+}
+
+.cell {
+    width: $size;
+    height: $size;
+    display: inline-block;
+    border: 1px solid #888;
+    margin: $spacing/2;
+
+    // zmiany nazw selektorów z &-dalsza-czesc-nazwy-klasy
+    &-1 {
+        &-1 {
+            background: darken( $first-color, 10 * $n);
+        };
+        &-2 {
+            background: darken( $first-color, 20 * $n);
+        };
+        &-3 {
+            background: darken( $first-color, 30 * $n);
+        }
+    }
+
+    &-2 {
+        &-1 {
+            background: darken($second-color, 10 *$n )
+        };
+        &-2 {
+            background: darken($second-color, 20 *$n )
+        };
+        &-3 {
+            background: darken($second-color, 30 *$n )
+        }
+    }
+
+    &-3 {
+        &-1 {
+            background: darken($third-color, 10 *$n )
+        };
+        &-2 {
+            background: darken($third-color, 20 *$n )
+        };
+        &-3 {
+            background: darken($third-color, 30 *$n )
+        }
+    }
+
+}
+
+
+</style>
