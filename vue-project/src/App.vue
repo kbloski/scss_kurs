5f792ba7c5eaefa5fa1065d6ecd4a9e10ab2b84e
Kamil Błoński <kblonski02@gmail.com>
2024-11-29 01:22:43 +0100

    Start kurs part sass

---
+++ vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        App
+    </div>
+    <sass-learn></sass-learn>
+
+</template>
+
+<script>
+import sassLearn from './components/sassLearn.vue';
+
+export default {
+    components: {
+        sassLearn
+    }
+}
+</script>
---
+++ vue-project/src/components/sassLearn.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>
+            Paragraph to styling
+        </p>
+    </div>
+</template>
+
+<style lang="scss">
+
+$text-color: #555;
+
+p {
+    color: $text-color;
+}
+
+</style>
