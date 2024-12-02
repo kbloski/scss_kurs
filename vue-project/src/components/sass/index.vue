aa465f0ee740727cfaf314e5a3f3ba854e5b5f32
Kamil Błoński <kblonski02@gmail.com>
2024-12-02 06:00:38 +0100

    if else sass

--- vue-project/src/components/sass/index.vue
+++ vue-project/src/components/sass/index.vue
@@ -9,12 +9,15 @@
     <!-- <l8_zagniezdzanie_pseudoklas></l8_zagniezdzanie_pseudoklas> -->
     <!-- <l9_tworzenie_nazw_za_pomoca_ampersand></l9_tworzenie_nazw_za_pomoca_ampersand> -->
     <!-- <l10_pseudoklasa_extend_i_placeholder></l10_pseudoklasa_extend_i_placeholder> -->
-    <l11_import_i_kompilacja_plikow></l11_import_i_kompilacja_plikow>
+    <!-- <l11_import_i_kompilacja_plikow></l11_import_i_kompilacja_plikow> -->
+    <l12_instrukcja_warunkowa_if></l12_instrukcja_warunkowa_if>
 </template>
 
 <script>
 import l10_pseudoklasa_extend_i_placeholder from './l10_pseudoklasa_extend_i_placeholder.vue';
 import l11_import_i_kompilacja_plikow from './l11_import_i_kompilacja_pilkow/l11_import_i_kompilacja_plikow.vue';
+import l12_instrukcja_warunkowa_if from './l12_instrukcja_warunkowa_if.vue';
+
 import l1Zmienne from './l1_zmienne.vue';
 import l2_zmienne_part2 from './l2_zmienne_part2.vue';
 import l3_listy from './l3_listy.vue';
@@ -38,7 +41,8 @@ export default {
         l8_zagniezdzanie_pseudoklas,
         l9_tworzenie_nazw_za_pomoca_ampersand,
         l10_pseudoklasa_extend_i_placeholder,
-        l11_import_i_kompilacja_plikow
+        l11_import_i_kompilacja_plikow,
+        l12_instrukcja_warunkowa_if
     }
 }
 </script>
---
+++ vue-project/src/components/sass/l12_instrukcja_warunkowa_if.vue
@@ -0,0 +1,42 @@
+<template>
+    <h1>Instrukcja wartunkowa if</h1>
+    <div></div>
+    <div class="div1"></div>
+</template>
+
+<style lang="scss" scoped>
+// $a: null;
+// $b: true;
+$a: 30;
+$b: 310;
+
+@function getColor($num1, $num2){
+    // @if($a or $b){
+    //     @return blue;
+    // }
+
+    @if($a > $b){
+        @return red;
+    }
+
+    @else if($a < $b){
+        @return green;
+    }
+
+    @return gray;
+}
+
+div {
+    aspect-ratio: 1/1;
+    width: 300px;
+    background-color: getColor($a, $b );
+    margin: 0 auto;
+}
+
+.div1 {
+    margin-top: 1rem;
+    // background-color: if(warunek, wartość jeśli prawda, wartość jeśli fałsz);
+    background-color: if($a > $b, green, red);
+}
+
+</style>
